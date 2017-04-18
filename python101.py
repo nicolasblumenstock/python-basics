c278b4a1f060202f9e59e96164495f91776de515
Nicolas Blumenstock <nico@Nicolass-MacBook-Pro.local>
2017-04-18 11:28:55 -0400

    This is my first commit.

---
+++ python101.py
@@ -0,0 +1,14 @@
+# print "Hello, World!"
+# print two different things on the same line
+print ("Hello, World", "Again")
+
+# this is won't work.
+# print "This 
+# is a couple lines of a sentence.
+ # Please Print."
+
+# This will for multiple lines.
+print """This
+will ignore the new lines
+until it sees another
+three double quotes"""
